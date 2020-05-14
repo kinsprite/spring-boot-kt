0d98fe46be6359ae85fdb4185de84eb74140c5c9
kinsprite <qxqs11@126.com>
2020-05-14 13:25:56 +0800

    add Dockerfile

--- demo-kt/build.gradle.kts
+++ demo-kt/build.gradle.kts
@@ -12,6 +12,7 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
+	maven { url = uri("https://maven.aliyun.com/repository/public/") }
 	mavenCentral()
 }
 
