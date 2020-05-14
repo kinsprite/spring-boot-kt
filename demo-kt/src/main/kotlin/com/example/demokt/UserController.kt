5ebc62b16a76d08682732feb0c693a6d302c97c1
kinsprite <qxqs11@126.com>
2020-05-14 11:48:59 +0800

    init repo

---
+++ demo-kt/src/main/kotlin/com/example/demokt/UserController.kt
@@ -0,0 +1,12 @@
+
+package com.example.demokt
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class UserController {
+
+    @GetMapping("/greeting")
+    fun greeting() = "Hello, World!"
+}
